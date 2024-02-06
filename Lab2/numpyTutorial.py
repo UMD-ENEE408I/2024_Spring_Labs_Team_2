d333c2a404b1c4d6cd09905745c42021ee1105b9
delsaess <95435068+delsaess@users.noreply.github.com>
2024-02-06 12:21:04 -0500

    Update numpyTutorial.py

--- Lab2/numpyTutorial.py
+++ Lab2/numpyTutorial.py
@@ -1,1 +1,15 @@
 
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy as scp
+import numpy as np
+import cv2
+#Secion 3.2
+print(np.ones((3,4)))
+print(np.zeros((4,3)))
+A = np.array([[1,2],[1,2],[1,2]])
+x = np.array([[1,2,3],[1,2,3]])
+B = np.matmul(A,x)
+print(B)
+V = np.array([[3,1],[1,2]])
+print(np.linalg.eig(V))
