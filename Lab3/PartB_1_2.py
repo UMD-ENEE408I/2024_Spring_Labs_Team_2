d676b215f49aac18f7ed69e7487c9f2a4db3a911
delsaess <95435068+delsaess@users.noreply.github.com>
2024-02-13 13:46:02 -0500

    Update PartB_1_2.py

--- Lab3/PartB_1_2.py
+++ Lab3/PartB_1_2.py
@@ -14,6 +14,21 @@ print("RMS value for M1.wav:", np.sqrt(np.mean((np.square(np.int64(m1_signal))))
 print("RMS value for M2.wav:", np.sqrt(np.mean((np.square(np.int64(m2_signal))))))
 print("RMS value for M3.wav:", np.sqrt(np.mean((np.square(np.int64(m3_signal))))))
 
+
+def convolution(input_signal, kernel):
+    input_len = len(input_signal)
+    kernel_len = len(kernel)
+    output_len = input_len + kernel_len - 1
+    output_signal = [0] * output_len
+
+    for i in range(output_len):
+        for j in range(kernel_len):
+            if i - j >= 0 and i - j < input_len:
+                output_signal[i] += (input_signal[i - j] * kernel[j])
+            
+        
+    return output_signal
+
 convolved = scp.signal.convolve(m1_signal,m2_signal)
 maxindex = np.argmax(convolved)
 #Index of Maximum Value
